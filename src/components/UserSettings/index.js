10348031f28f7b3290421dd3afd918b830b82147
Gregfanyan <grigorfanyan@gmail.com>
2022-06-22 13:01:41 +0200

    ⚰️ removed unused style

--- src/components/UserSettings/index.js
+++ src/components/UserSettings/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./UserSettings.module.css";
 function UserSettings() {
-  return <div className={styles.userSettingsContainer}></div>;
+  return <div className={styles.userSettingsContainer}>user settings</div>;
 }
 
 export default UserSettings;
