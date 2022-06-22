5a7b7eb3a123c49e8007d93a3a20de7550474b3b
Gregfanyan <grigorfanyan@gmail.com>
2022-06-22 20:15:00 +0200

    💄 improved modal structure, added missing elements

--- src/components/FormModal/index.js
+++ src/components/FormModal/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import style from "./FormModal.module.css";
 
@@ -8,23 +8,60 @@ function FormModal() {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const changePassword = "E-Mail-Adresse Ändern";
   return (
     <>
       <Button className={style.button} onClick={handleShow}>
-        E-Mail-Adresse Ändern
+        {changePassword}
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>{changePassword}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+        <Modal.Body>
+          Bitte gib deine neue E-Mail-Adresse zweimal an. Deine alte
+          E-Mail-Adresse wird damit für das Portal ungültig. Bei Angeboten oder
+          Fragen werden wir dich künfig über die neue E-Mail-Adresse
+          kontaktieren. Um wieder alle Funktionen des Portals nutzen zu können,
+          musst du deine neue E-Mail-Adresse bestätigen.
+        </Modal.Body>
+
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="email"
+                placeholder="Email-Addresse"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="email"
+                placeholder="Email-Addresse wiederholen"
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer
+          style={{
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            color: "#ad325d",
+          }}
+        >
+          <Button
+            style={{
+              backgroundColor: "#ad325d",
+            }}
+            variant="primary"
+            onClick={handleClose}
+          >
+            speichern
           </Button>
         </Modal.Footer>
       </Modal>
